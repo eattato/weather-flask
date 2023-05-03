7cc926e290f0b1d1c99677b73eda0a10ee40f7c6
eattato <80892781+eattato@users.noreply.github.com>
2023-05-03 13:00:34 +0900

    dto에서 코드 처리하게 바꿔서 여기서 뺐음

--- server.py
+++ server.py
@@ -11,24 +11,20 @@ app = Flask(__name__)
 # 요청 처리
 @app.route("/predict", methods=["POST"])
 def predict():
+    print("요청 받음")
+    print(request.get_data())
     if request.is_json:
-        locationCodes = {
-               '강원도': 0, '경기도': 1, '인천광역시': 11, '서울특별시': 8, '경상북도': 3, '충청북도': 16, '충청남도': 15, '대전광역시': 6, '전라북도': 13, '대구광역시': 5, '울산광역시': 10, '경상남도': 2, '광주광역시': 4, '부산광역시': 7, '전라남도': 12, '제주도': 14, '세종특별자치시': 9
-               }
         data = request.get_json()
+        test_x = [data["locationCode"], data["month"], data["temperature"], data["humidity"]]
+        test_x = np.array([test_x])
+        res = model.predict(test_x)
 
-        if data["location"] in locationCodes:
-            loc = locationCodes[data["location"]]
-            test_x = [loc, data["month"], data["temperature"], data["humidity"]]
-            test_x = np.array([test_x])
-            res = model.predict(test_x)
-
-            res = {
-                "rain": str(res[0][0]),
-                "snow": str(res[0][1])
-            }
-            print(res)
-            return jsonify(res)
+        res = {
+            "rain": str(res[0][0]),
+            "snow": str(res[0][1])
+        }
+        print(res)
+        return jsonify(res)
     return "prediction"
 
 # 메인
